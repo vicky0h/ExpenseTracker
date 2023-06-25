b558c5b7b1498380e6812f3f32a58f1ae7587c38
Vicky H <kengtania@gmail.com>
2023-06-25 21:08:58 +0800

    updated baseURL for render

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import axios from 'axios';
 
 // const BASE_URL = "http://127.0.0.1:5000/api/v1/";
-const BASE_URL = "https://expense-tracker-qbm3.onrender.com/";
+const BASE_URL = "https://expense-tracker-qbm3.onrender.com/api/v1";
 
 const GlobalContext = React.createContext();
 
