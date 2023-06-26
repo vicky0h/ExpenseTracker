58dbdae5631236f7caae5e0edfe0931fa82312d0
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-26 11:31:28 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-// readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
 // readdirSync('./routes').map((route) => app.use('https://expense-tracker-eight-iota.vercel.app/', require('./routes/' + route)));
-readdirSync('./routes').map((route) => app.use('https://expense-tracker-eight-iota.vercel.app/api/v1', require('./routes/' + route)));
+// readdirSync('./routes').map((route) => app.use('https://expense-tracker-eight-iota.vercel.app/api/v1', require('./routes/' + route)));
 
 const server = () => {
     db();
