284c68ca497c161b6e01af736831dfc72fed1a9a
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-26 10:15:58 +0800

    Update app.js backend readdirSync path

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,9 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-readdirSync('backend/routes').map((route) => app.use('/api/v1', require('backend/routes/' + route)));
+// readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
 // readdirSync('./routes').map((route) => app.use('https://expense-tracker-eight-iota.vercel.app/', require('./routes/' + route)));
+readdirSync('./routes').map((route) => app.use('https://expense-tracker-eight-iota.vercel.app/api/v1', require('./routes/' + route)));
 
 const server = () => {
     db();
