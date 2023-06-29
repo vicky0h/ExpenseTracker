e435c06f7a5f668bd231b0a23d736dcc47327316
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-29 08:44:40 +0800

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-// const mongoURL = `mongodb://kengtania:kengtania@ac-fhsnu55-shard-00-00.ebgbywk.mongodb.net:27017,ac-fhsnu55-shard-00-01.ebgbywk.mongodb.net:27017,ac-fhsnu55-shard-00-02.ebgbywk.mongodb.net:27017/?ssl=true&replicaSet=atlas-v5si54-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
 
 const db = async () => {
     try {
         mongoose.set('strictQuery', false);
-        // await mongoose.connect(mongoURL);
         await mongoose.connect(process.env.MONGO_URL);
         console.log('DB Connection Sucessful');
     } catch (error) {
